a7a0813deb6e4919445b747d2dd094de35a99dba
gega gagua <gegagagua@gmail.com>
2025-07-01 03:00:41 +0400

    token fix

--- routes/api.php
+++ routes/api.php
@@ -4,19 +4,22 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CityController;
 use App\Http\Controllers\Api\GardenController;
-
+use App\Http\Middleware\ForceJsonResponse;
 use App\Http\Controllers\Api\AuthController;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
+Route::middleware([ForceJsonResponse::class])->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
+
+    Route::get('/gardens', [GardenController::class, 'index']);
+
+    Route::get('/cities', [CityController::class, 'index']);
 });
-
-Route::get('/gardens', [GardenController::class, 'index']);
-
-Route::get('/cities', [CityController::class, 'index']);
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
