bbe05525ab1541da89db1111137700db3d29d590
gega gagua <gegagagua@gmail.com>
2025-07-19 22:59:21 +0400

    cards count

--- app/Models/GardenGroup.php
+++ app/Models/GardenGroup.php
@@ -18,7 +18,7 @@ class GardenGroup extends Model
 
     public function cards()
     {
-        return $this->hasMany(Card::class);
+        return $this->hasMany(Card::class, 'group_id');
     }
 
     public function parents()
