c917f1fcc030c391094c7d31fefd7208811933cb
gega gagua <gegagagua@gmail.com>
2025-09-26 17:38:49 +0400

    countries seed

---
+++ database/seeders/UpdateCountriesDefaultValuesSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Country;
+
+class UpdateCountriesDefaultValuesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Update all countries with default values
+        Country::query()->update([
+            'currency' => 'USD',
+            'garden_percent' => 5.00,
+            'language' => 'en',
+            'tariff' => 10.00,
+            'exchange_rate' => 1.00,
+        ]);
+
+        echo "Updated all countries with default values:\n";
+        echo "- Currency: USD\n";
+        echo "- Garden percent: 5%\n";
+        echo "- Language: en\n";
+        echo "- Tariff: 10\n";
+        echo "- Exchange rate: 1.00\n";
+    }
+}
