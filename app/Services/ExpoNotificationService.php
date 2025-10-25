0a1afc879cfc5c6bc2903166be9a992f9652aa25
gega gagua <gegagagua@gmail.com>
2025-10-25 21:48:00 +0400

    change history status

--- app/Services/ExpoNotificationService.php
+++ app/Services/ExpoNotificationService.php
@@ -334,11 +334,6 @@ class ExpoNotificationService
                 }
             }
 
-            // Log the complete payload being sent to Expo
-            \Log::info('ExpoNotificationService: Complete payload to Expo', [
-                'payload' => $payload
-            ]);
-
             $response = Http::withHeaders([
                 'Accept' => 'application/json',
                 'Accept-encoding' => 'gzip, deflate',
