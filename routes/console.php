a0c052553ba76c9aa3313955f4754f559e8c6ac0
kobasisauri <kobasisauri@gmail.com>
2026-05-07 19:57:54 +0400

    Reconcile every 5 minutes instead of 2

--- routes/console.php
+++ routes/console.php
@@ -9,6 +9,6 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('procredit:reconcile')
-    ->everyTwoMinutes()
+    ->everyFiveMinutes()
     ->withoutOverlapping()
     ->runInBackground();
